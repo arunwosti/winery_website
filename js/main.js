988bcbd48d9e71f17bbab876df90634403e9af6e
arunwosti <ostiarun55@gmail.com>
2022-02-24 10:48:12 +0545

    Winery Corporate Website

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// active navbar
+/*let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 30){
+        document.querySelector('.scroll-on').style.display='block'
+        document.querySelector('.navbar').style.display='block'
+      
+        nav.classList.add("scroll-on");
+    }
+    else{
+        document.querySelector('.scroll-on').style.display='none'
+        document.querySelector('.navbar').style.display='block'
+        nav.classList.remove("scroll-on");
+
+    }
+}*/
+
+
+//nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+//counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(()=> {
+            current += increment;
+            obj.textContent= current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step);
+    }
+    counter("count1",500,1287,3000);
+    counter("count2",1000,1287,3000);
+    counter("count3",0,1287,3000);
+    counter("count4",5,1287,3000);
+});
+
